9c52678844fe4d226e37df154ae5e9c4482cfff8
chiucs <chiucs@chipright.com.tw>
2022-05-03 22:16:15 +0800

    learn btn and layout

--- hello.py
+++ hello.py
@@ -19,9 +19,15 @@ class MyFrame(wx.Frame):
         self.statusBar.SetStatusText("ChiuCS ERP")
         self.SetStatusBar(self.statusBar)
 
-        x=wx.Panel(self)
-        wx.Button(x, label="Click ME!", pos=(10,10), size=(100,100))
-
+        #x=wx.Panel(self)
+        self.btn1=wx.Button(self, label="Click btn1!")
+        self.Bind(wx.EVT_BUTTON, self.OnClock, self.btn1)
+        self.btn2=wx.Button(self, label="Click btn2!")
+        self.Bind(wx.EVT_BUTTON, self.OnClock, self.btn2)
+        x=wx.BoxSizer(wx.VERTICAL)
+        x.Add(self.btn1)
+        x.Add(self.btn2)
+        self.SetSizer(x)
         self.Centre()
     def onQuit(self, event):
         ret=wx.MessageBox("Are You Sure?","QUIT!",wx.CANCEL|wx.OK|wx.ICON_QUESTION)
@@ -32,6 +38,20 @@ class MyFrame(wx.Frame):
     def onInfo(self, e):
         n=self.statusBar.GetFieldsCount()
         print(n)
+    def OnClock(self, e):
+        print('Clock!')
+        print(self)
+        obj=e.GetEventObject()
+        print(obj.GetLabel())
+        if obj == self.btn1:
+            print('btn1')
+        print(e.GetId())
+        print(e.GetEventType())
+        print(wxEVT_BUTTON)
+        #ret = wx.MessageBox("Clicked", "Info!", wx.OK|wx.ICON_EXCLAMATION)
+        #ret = wx.MessageBox("Clicked", "Info!", wx.OK|wx.ICON_ERROR)
+        #ret = wx.MessageBox("Clicked", "Info!", wx.OK|wx.ICON_INFORMATION)
+        #ret = wx.MessageBox("Clicked", "Info!", wx.OK|wx.ICON_EXCL)
 
 def main():
     app=wx.App()
